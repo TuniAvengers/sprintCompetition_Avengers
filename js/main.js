6a6da885b306dab306622b686069d4085e489071
AF0ns3ca <afonsecahdez@gmail.com>
2023-12-11 10:48:51 +0100

    json working

--- js/main.js
+++ js/main.js
@@ -1,29 +1,28 @@
-const productJSON = './producto.JSON';
-const display = document.querySelector('#display');
-
-const getData = async () => {
-    const response = await fetch(productJSON);
-    const data = await response.json();
-    return data;
-};
-
-const displayProducts = async () => {
-    const payload = await getData();
-    
-    let dataDisplay = payload.map((item) => {
-        const{nombre,precio, peso, imagen} = item;
-
-        return `
-            <div class="card">
-                <p>${nombre}</p>
-                <p>${precio}</p>
-                <p>${peso}</p>
-            </div>
-
-        `;
+// Fetch the JSON file
+fetch("js/producto.json")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+    .catch(function (err) {
+        console.log("error: " + err);
     });
-
-    display.innerHTML = dataDisplay;
-};
-
-displayProducts();
+ 
+// Append the data to the DOM
+function appendData(data) {
+    const mainContainer = document.querySelector(".display");
+    for (let i = 0; i < data.productos.length; i++) {
+        const div = document.createElement("div");
+        div.innerHTML = `<div class="card">
+        <img src="${data.productos[i].imagen}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${data.productos[i].nombre}</h5>
+          <p class="card-text">${data.productos[i].precio}</p>
+          <a href="#" class="btn btn-primary">Comprar</a>
+        </div>
+      </div>`;
+        mainContainer.appendChild(div);
+    }
+}
