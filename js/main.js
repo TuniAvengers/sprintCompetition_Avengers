a0ea6c692a876fbd7021786a87d2e1640a661796
AF0ns3ca <afonsecahdez@gmail.com>
2023-12-12 12:01:04 +0100

    basket items

--- js/main.js
+++ js/main.js
@@ -1,51 +1,85 @@
 
 //SLIDER
-document.addEventListener("DOMContentLoaded", function() {
-    const images = document.querySelectorAll('.slider-img');
-    let currentIndex = 0;
+document.addEventListener("DOMContentLoaded", function () {
+  const images = document.querySelectorAll(".slider-img");
+  let currentIndex = 0;
 
-    function showImage(index) {
-        images.forEach((image, i) => {
-            if (i === index) {
-                image.classList.add('active');
-            } else {
-                image.classList.remove('active');
-            }
-        });
-    }
+  function showImage(index) {
+    images.forEach((image, i) => {
+      if (i === index) {
+        image.classList.add("active");
+      } else {
+        image.classList.remove("active");
+      }
+    });
+  }
 
-    function nextImage() {
-        currentIndex = (currentIndex + 1) % images.length;
-        showImage(currentIndex);
-    }
+  function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+  }
 
-    setInterval(nextImage, 5000);
+  setInterval(nextImage, 5000);
 });
 
-
 document.addEventListener("DOMContentLoaded", function () {
-    function addOrder(id, nombre, precio, peso) {    
-        const total = document.getElementById("total");
-        const totalText = document.getElementById("total-text");
-        totalText.classList.remove("hide");
-        const listItem = document.createElement("li");
-        //añadimos el id del producto al elemento li
-        listItem.setAttribute("id", id);
-        listItem.innerHTML = `
+
+    function basketContent(){
+        const full  = document.getElementById("full-basket");
+        const empty = document.getElementById("empty-basket");
+        if(itemsTotal() < 1){
+            full.classList.add("hide");
+            empty.classList.remove("hide");
+        } else{
+            full.classList.remove("hide");
+            empty.classList.add("hide");
+        }
+    }
+
+  function addOrder(id, nombre, precio, peso) {
+    //const total = document.getElementById("total");
+    const totalText = document.getElementById("total-text");
+    totalText.classList.remove("hide");
+    const listItem = document.createElement("li");
+    //añadimos el id del producto al elemento li
+    listItem.setAttribute("id", id);
+    listItem.classList.add("product");
+    listItem.innerHTML = `
             <p>${nombre}</p>
             <p>${peso} kg</p>
-            <p>${precio}€</p>
+            <p class="precio">${precio}€</p>
             <button class="btn btn-danger">x</button>
         `;
-        listItem.querySelector("button").addEventListener("click", function () {
-            eliminarElementoPorId(id);
-        });
-        
-    
-        document.getElementById("list-order").appendChild(listItem);
-        let precioTotal = parseFloat(document.getElementById("total").innerHTML) + precio;
-        total.innerHTML = precioTotal.toFixed(2);
-    }
+    listItem.querySelector("button").addEventListener("click", function () {
+      eliminarElementoPorId(id);
+      precioTotal();
+      document.getElementById("counter").innerHTML = itemsTotal();
+      if (itemsTotal() < 1) {
+        document.getElementById("order").classList.add("hide");
+      }
+      basketContent();
+    });
+
+    document.getElementById("list-order").appendChild(listItem);
+    precioTotal();
+    document.getElementById("counter").innerHTML = itemsTotal();
+    basketContent();
+  }
+
+  function precioTotal() {
+    let precioTotal = 0;
+    const precios = document.querySelectorAll(".precio");
+    precios.forEach((precio) => {
+      precioTotal += parseFloat(precio.innerHTML);
+    });
+    document.getElementById("total").innerHTML = precioTotal.toFixed(2);
+  }
+
+  function itemsTotal() {
+    //calculamos el numero de elementos con clase producto
+    const items = document.querySelectorAll(".product");
+    return items.length;
+  }
   // Fetch the JSON file
   fetch("js/producto.json")
     .then(function (response) {
@@ -72,26 +106,28 @@ document.addEventListener("DOMContentLoaded", function () {
           <p class="card-text">${data.productos[i].precio}€ IVA incl.</p>
           <button class="btn btn-primary">Comprar</button>
         </div>`;
-        div.querySelector("button").addEventListener("click", function () {
-            addOrder(data.productos[i].id, data.productos[i].nombre, parseFloat(data.productos[i].precio), parseFloat(data.productos[i].peso));
-        });
+      div.querySelector("button").addEventListener("click", function () {
+        addOrder(
+          data.productos[i].id,
+          data.productos[i].nombre,
+          parseFloat(data.productos[i].precio),
+          parseFloat(data.productos[i].peso)
+        );
+      });
       mainContainer.appendChild(div);
     }
   }
-
-  
 });
 
-
 function eliminarElementoPorId(id) {
-    const elemento = document.getElementById(id);
+  const elemento = document.getElementById(id);
 
-    if (elemento) {
-        // Si el elemento existe, lo eliminamos
-        elemento.parentNode.removeChild(elemento);
-    } else {
-        console.log(`No se encontró un elemento con el id ${id}`);
-    }
+  if (elemento) {
+    // Si el elemento existe, lo eliminamos
+    elemento.parentNode.removeChild(elemento);
+  } else {
+    console.log(`No se encontró un elemento con el id ${id}`);
+  }
 }
 
 document.getElementById("basket").addEventListener("click", function () {
